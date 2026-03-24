6a025bfc70809752a192391b8149b8d11d0ef460
rontratistas-eng <rontratistas@gmail.com>
2026-03-24 11:49:41 -0500

    Update scriptservicioexperiencia.js

--- scriptservicioexperiencia.js
+++ scriptservicioexperiencia.js
@@ -22,23 +22,3 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// Animación simple de entrada
-const style = document.createElement('style');
-style.innerHTML = `
-    @keyframes fadeIn {
-        from { opacity: 0; transform: translateY(10px); }
-        to { opacity: 1; transform: translateY(0); }
-    }
-`;
-document.head.appendChild(style);
-
-const mobileMenu = document.getElementById('mobile-menu');
-const navLinks = document.querySelector('.nav-links');
-
-mobileMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    // Cambia el icono de barras a una X
-    const icon = mobileMenu.querySelector('i');
-    icon.classList.toggle('fa-bars');
-    icon.classList.toggle('fa-times');
-});
